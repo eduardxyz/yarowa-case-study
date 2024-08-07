556e5c70878f19b47373309761d52ee7d46a7603
Eduard Marian <55080847+eduardxyz@users.noreply.github.com>
2024-08-07 17:39:03 +0300

    Adds ReactJS webapp for case study

---
+++ app/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import yarowaLogo from './assets/yarowa.png'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.yarowa.com/english/home/" target="_blank">
+          <img src={yarowaLogo} className="logo yarowa" alt="Yarowa logo" />
+        </a>
+      </div>
+      <h1>Hello Yarowa AG!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
